088f1fa8d6d567e96afe19c1e4dfd1fb9b4fed71
KMKINGMAN <kingkurkar@gmail.com>
2025-06-13 22:21:44 +0300

    Fix GitHub Pages deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: '',
+  basePath: '/training-plan/',
   eslint: {
     ignoreDuringBuilds: true,
   },
